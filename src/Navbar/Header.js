8c481002c2e5bf4717765a34070a323ff7a93522
Chandan <Chandan17ee@gmail.com>
2023-01-28 01:52:54 +0530

    feat : added new file

--- src/Navbar/Header.js
+++ src/Navbar/Header.js
@@ -5,14 +5,28 @@ const NavBar = () => {
   return (
     <ul className="navigation">
       <li>
-       <a href> <NavLink to="/HTMLForm">HTML Form</NavLink>{" "}</a>
+        <a href>
+          {" "}
+          <NavLink to="/HTMLForm">HTML Form</NavLink>{" "}
+        </a>
       </li>
       <li>
-    <a href>    <NavLink to="/BootstrapForm">Bootstrap Form</NavLink>{" "}</a>
+        <a href>
+          {" "}
+          <NavLink to="/BootstrapForm">Bootstrap Form</NavLink>{" "}
+        </a>
       </li>
       <li>
         {" "}
-        <a href><NavLink to="/validateForm">Validated Form</NavLink></a>
+        <a href>
+          <NavLink to="/validateForm">Validated Form</NavLink>
+        </a>
+      </li>
+      <li>
+        {" "}
+        <a href>
+          <NavLink to="/UploadFile">Upload File</NavLink>
+        </a>
       </li>
     </ul>
   );
